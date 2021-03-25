a03b51aef9c77df24c9828deda7077da96ab69d4
susanwulfhawes <62613493+susanwulfhawes@users.noreply.github.com>
2021-03-25 18:55:13 -0400

    temp

--- red-badge-demo-ts/src/App.tsx
+++ red-badge-demo-ts/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import FunctionalClickCounter from "./component/FunctionalClickCounter";
+import FunctionalClickCounter from "./components/FunctionalClickCounter";
+import ClassClickCounter from "./components/ClassClickCounter"
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <FunctionalClickCounter initialValue={6} />
       <FunctionalClickCounter initialValue={10} />
       <FunctionalClickCounter initialValue={4} />
+      <ClassClickCounter />
     </div>
   );
 }
--- red-badge-demo-ts/src/components/ClassClickCounter.jsx
+++ red-badge-demo-ts/src/components/ClassClickCounter.jsx
@@ -1,1 +1,21 @@
 // Create a class version of the Functional Component
+import React, { Component } from 'react';
+
+class ClassClickCounter extends Component {
+    constructor(props){
+        super(props);
+        this.state = {countValue: 5}
+    }
+
+
+    render() {
+        return (
+            <div>
+                Hi from classclickcounter
+                <button onClick={this.handleClick}>Click Me From Class</button>
+            </div>
+        )
+ }
+}
+
+export default ClassClickCounter;
