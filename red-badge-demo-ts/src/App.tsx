fd45072674a022a0954386ec841b5f35b6980ba6
susanwulfhawes <62613493+susanwulfhawes@users.noreply.github.com>
2021-03-25 21:19:03 -0400

    finished

--- red-badge-demo-ts/src/App.tsx
+++ red-badge-demo-ts/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import FunctionFetchOnload from "./components/FunctionFetchOnload";
+import ClassFetchOnload from "./components/ClassFetchOnload"
 
 function App() {
   return (
     <div>
       <FunctionFetchOnload />
+      <ClassFetchOnload />
     </div>
   );
 }
